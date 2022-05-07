3428b32aad20af8f90e4c9060f36b0302a6dc1df
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-05-07 19:49:17 +0200

    Update Binary Tree Level Order Traversal II.java

--- BFS/Binary Tree Level Order Traversal II/Binary Tree Level Order Traversal II.java
+++ BFS/Binary Tree Level Order Traversal II/Binary Tree Level Order Traversal II.java
@@ -1,1 +1,40 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+import java.util.Collections;
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null ) return res;
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                if(q.peek().left != null)
+                    q.offer(q.peek().left);
+                if(q.peek().right != null)
+                    q.offer(q.peek().right);
+                list.add(q.poll().val);
+            }
+            res.add(list);
+        }
+      Collections.reverse(res);
+       return res;
+    }
+}
