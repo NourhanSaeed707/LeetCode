25bc329ef7d483928e1161b95b75e4eb978c9632
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-05-07 20:19:38 +0200

    Update Minimum Depth of Binary Tree.cpp

--- BFS/Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.cpp
+++ BFS/Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.cpp
@@ -1,1 +1,37 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    queue<TreeNode*> q;
+    int BFS(TreeNode* root){
+        int level = 0;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            level++;
+            while(n--){
+                TreeNode* temp = q.front();
+                q.pop();
+                if(temp->left ) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+              // to return shortest leaf that has no childern 
+                if(temp->left == NULL && temp->right == NULL) return level;
+            }
+        }
+        return level;
+    }
+    int minDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        return BFS(root);
+    }
+};
