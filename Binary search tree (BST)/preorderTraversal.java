14fa30f78488d3b8309959d53b7707dce47e0372
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-05-09 02:52:28 +0200

    Update preorderTraversal.java

--- Binary search tree (BST)/preorderTraversal.java
+++ Binary search tree (BST)/preorderTraversal.java
@@ -1,1 +1,12 @@
-
+//in preorderTraversal (we visit root then left nodes then right nodes).
+class Solution {
+    ArrayList inorder = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+         if(root != null){
+            inorder.add(root.val);
+            inorderTraversal(root.left);
+            inorderTraversal(root.right);
+        }
+        return inorder;
+    }
+}
