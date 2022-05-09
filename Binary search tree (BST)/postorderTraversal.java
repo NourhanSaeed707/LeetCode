f9f19f7a8aa465497f47ef3d6b6d386edaa9be31
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-05-09 02:55:06 +0200

    Update postorderTraversal.java

--- Binary search tree (BST)/postorderTraversal.java
+++ Binary search tree (BST)/postorderTraversal.java
@@ -1,1 +1,13 @@
 
+//in postorderTraversal (we visit left nodes then right nodes then root).
+class Solution {
+    ArrayList postorder = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+         if(root != null){
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            postorder.add(root.val);
+        }
+        return postorder;
+    }
+}
