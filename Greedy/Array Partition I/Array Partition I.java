6c1f5c8167517fcfe8184ad0f78d3a79c217e1a7
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-05-13 12:55:31 +0200

    Update Array Partition I.java

--- Greedy/Array Partition I/Array Partition I.java
+++ Greedy/Array Partition I/Array Partition I.java
@@ -1,1 +1,15 @@
 
+import java.util.*;
+class Solution {
+    public int arrayPairSum(int[] nums) {
+      int sum = 0;
+      Arrays.sort(nums);
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(i - 1 >= 0){
+                sum += Math.min(nums[i], nums[i-1]);
+                i--;
+            }
+        }  
+        return sum;
+    }
+}
