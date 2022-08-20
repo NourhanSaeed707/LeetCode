3261fc03f8e56d9f6c4911ee1dc6a20ee77e1faa
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-08-20 16:52:59 +0200

    Update Find the Winner of the Circular Game.cpp

--- Stack/Stack/Find the Winner of the Circular Game/Find the Winner of the Circular Game.cpp
+++ Stack/Stack/Find the Winner of the Circular Game/Find the Winner of the Circular Game.cpp
@@ -1,1 +1,24 @@
 
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        vector <int> vec;
+        int i = 0, count = 1, winner;
+        for(int i = 1; i <= n; i++)
+           vec.push_back(i);
+        while( vec.size() > 1 ){
+            if( count == k ){
+                vec.erase( vec.begin() + i);
+                count = 0;
+                i--;
+            }
+            count++, i++;
+            if(i >= vec.size()) i = 0;
+        }
+        for(int i = 0; i < n; i++){
+            if(vec[i] != -1)
+                winner = vec[i]; break;
+        } 
+        return winner;
+    }
+};
