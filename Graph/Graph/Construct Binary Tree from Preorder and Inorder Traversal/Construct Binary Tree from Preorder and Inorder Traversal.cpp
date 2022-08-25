4eb524d02305b8d07261f902f09f4c533fb59310
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-08-25 11:54:58 +0200

    Update Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Graph/Graph/Construct Binary Tree from Preorder and Inorder Traversal/Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Graph/Graph/Construct Binary Tree from Preorder and Inorder Traversal/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,1 +1,33 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int preIndex;
+    unordered_map <int,int> inorderIndexMap;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+       preIndex = 0;
+       for(int i = 0; i < inorder.size(); i++)
+           inorderIndexMap[ inorder[i] ] = i;
+      // for(int i = 0; i < inorder.size(); i++)
+         //  inorderIndexMap[ inorder[i] ] = 0;
+        return arrayToTree(preorder, 0, inorder.size() - 1);
+    }
+    TreeNode* arrayToTree(vector<int>& preorder, int left, int right){
+        if(left > right ) return NULL;
+        TreeNode* root = new TreeNode(preorder[preIndex]);
+        preIndex++;
+        root->left = arrayToTree(preorder,left, inorderIndexMap[root->val] - 1);
+        root->right =arrayToTree(preorder, inorderIndexMap[root->val] + 1, right);
+        return root;
+    }
+};
