c494b42e78243db91f2e71c5197a83eaa0c10bc5
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-09-02 10:26:50 +0200

    Update Path Sum II.java

--- Binary search tree (BST)/Path Sum II/Path Sum II.java
+++ Binary search tree (BST)/Path Sum II/Path Sum II.java
@@ -1,1 +1,33 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> paths = new ArrayList<>();
+        findPath(root, targetSum , new ArrayList<Integer>(), paths);
+        return paths;
+    }
+    public void findPath(TreeNode root, int sum , List<Integer>current, List<List<Integer>>paths ){
+        if(root == null) return;
+        current.add(root.val);
+        if(root.val == sum && root.left == null && root.right == null){
+            paths.add(current);
+            return;
+        }
+        findPath(root.left, sum - root.val , new ArrayList<Integer>(current), paths);
+        findPath(root.right, sum - root.val , new ArrayList<Integer>(current), paths);
+    }
+}
