e04f1ac5d38a2fc7421f954974532b9cc646a7cd
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-09-10 16:18:55 +0200

    Update Keys and Rooms.java

--- graph/Keys and Rooms/Keys and Rooms.java
+++ graph/Keys and Rooms/Keys and Rooms.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        ArrayDeque<Integer> que = new ArrayDeque<>();
+        que.push(0);
+        HashSet<Integer> set = new HashSet<>();
+        int n = rooms.size();
+        boolean[] visited = new boolean[n];
+        while(que.size()>0){
+            int rem = que.removeFirst();
+            visited[rem] = true;
+            set.add(rem);
+            for(int k:rooms.get(rem)){
+                if(visited[k]==false)
+                    que.push(k);
+            }
+        }
+        return set.size()==n;
+    }
+}
