dfd7baba5b4751b26daa4546def19e238a7b7099
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-09-10 15:45:26 +0200

    Update Keys and Rooms.cpp

--- graph/Keys and Rooms/Keys and Rooms.cpp
+++ graph/Keys and Rooms/Keys and Rooms.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<bool> visit(rooms.size(), false);
+        visit[0] = true;
+        int i = 0;
+        queue<int> q;
+        while( i < rooms.size() ) {
+            for(int j = 0; j < rooms[i].size();j++){
+                if(visit[rooms[i][j]] == false){
+                   q.push(rooms[i][j]);
+                     visit[rooms[i][j]] = true;
+                }
+            }
+            i = q.front();
+            q.pop();
+        }
+        for(int i = 0; i < visit.size(); i++)
+            if(visit[i] == false) return false;
+        return true;
+    }
+};
