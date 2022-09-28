cd13b207543259b6f96aa9e8f57e59c3d4e45cd1
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2022-09-28 19:22:51 +0200

    Update K Closest Points to Origin.cpp

--- Heap/K Closest Points to Origin/K Closest Points to Origin.cpp
+++ Heap/K Closest Points to Origin/K Closest Points to Origin.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+  
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector< vector<int>> v;
+        for(int i = 0; i < points.size(); i++){
+            int dis = pow(points[i][0], 2) + pow(points[i][1], 2);
+            v.push_back({dis, i});
+        }
+        sort(v.begin(), v.end());
+        vector<vector<int>> ans;
+       for(int i = 0; i<k ; i++){
+            ans.push_back(points[v[i][1]]);
+        }
+        
+        return ans;
+    }
+};
