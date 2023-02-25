730e00906e12a3e01903c2b79ac3f8970a521ff1
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2023-02-25 21:48:13 +0200

    Update N-ary Tree Preorder Traversal.java

--- Tree/N-ary Tree Preorder Traversal/N-ary Tree Preorder Traversal.java
+++ Tree/N-ary Tree Preorder Traversal/N-ary Tree Preorder Traversal.java
@@ -1,1 +1,35 @@
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> list = new ArrayList<Integer>();
+    public void preorderHelper(Node node){
+        if(node.children == null) return;
+        list.add(node.val);
+        for(Node child: node.children){
+             preorderHelper(child);
+        }
+    }
+    public List<Integer> preorder(Node root) {
+        if(root == null) return list;
+         preorderHelper(root);
+         return list;
+    }
+}
