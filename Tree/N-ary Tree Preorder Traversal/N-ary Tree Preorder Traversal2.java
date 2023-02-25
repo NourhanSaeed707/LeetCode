12850eaba0a6bf923dad67d145d0388ca463a8d7
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2023-02-25 22:15:36 +0200

    Update N-ary Tree Preorder Traversal2.java

--- Tree/N-ary Tree Preorder Traversal/N-ary Tree Preorder Traversal2.java
+++ Tree/N-ary Tree Preorder Traversal/N-ary Tree Preorder Traversal2.java
@@ -1,1 +1,18 @@
 
+class Solution {
+    List<Integer> list = new ArrayList<Integer>();
+    public List<Integer> preorder(Node root) {
+        if(root == null) return list;
+         Stack<Node> stack = new Stack<Node>();
+         stack.push(root);
+         while(!stack.isEmpty()){
+            Node node = stack.pop();
+            list.add(node.val);
+            // Pushing children in reverse order. 
+            for(int i = node.children.size() -1; i >= 0; i--){
+                stack.push(node.children.get(i));
+            }
+         }
+         return list;
+    }
+}
