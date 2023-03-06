428c4aea718956cc1560ffc26e19deba9b2cc822
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2023-03-06 21:52:24 +0200

    Update Decode String.java

--- Stack/Stack/Decode String/Decode String.java
+++ Stack/Stack/Decode String/Decode String.java
@@ -1,1 +1,36 @@
-
+class Solution {
+    public String decodeString(String s) {
+        Stack<Character> stack = new Stack<>();
+        String res = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) != ']')
+                stack.push(s.charAt(i));
+            else{
+                StringBuilder substring  = new StringBuilder("");
+                while(stack.peek() != '['){
+                    substring.append( stack.pop());
+                }
+                substring.reverse();
+                stack.pop();
+                StringBuilder k = new StringBuilder();
+                while(!stack.isEmpty() && Character.isDigit(stack.peek())){
+                    k.append(stack.pop());
+                }
+                k.reverse();
+                for(int x = 0; x < Integer.parseInt(k.toString()); x++){
+                 for(int j = 0; j < substring.length(); j++){
+                    stack.push(substring.charAt(j));
+                  }
+                }
+            }
+        }
+        StringBuilder r = new StringBuilder();
+        while(!stack.isEmpty()){
+         if(Character.isDigit(stack.peek())) stack.pop();
+         if(!stack.isEmpty())
+          r.append(stack.pop());
+        }
+        r.reverse();
+        return r.toString();
+    }
+}
