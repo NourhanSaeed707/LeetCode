146aa5d31e65ec732aaf248b08391583eedede7d
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2023-03-17 18:49:49 +0200

    Create Basic Data Type2.java

---
+++ Programming Skills/Basic Data Type/Basic Data Type2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int count = 0;
+        if(low % 2 != 0)  count++;
+        else if(high % 2 != 0)  count++;
+       return (((high-low)/2)+count) ;
+    }
+}
