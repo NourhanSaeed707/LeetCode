fec0bd47c60257bf967ad009509ac3caa2fd3feb
Nourhan saeed <64387352+NourhanSaeed707@users.noreply.github.com>
2023-03-17 18:43:06 +0200

    Update Relative Ranks.java

--- Array/Relative Ranks/Relative Ranks.java
+++ Array/Relative Ranks/Relative Ranks.java
@@ -1,1 +1,27 @@
-
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int arr[] = new int[score.length];
+        for(int i =0; i < score.length; i ++)
+            arr[i] = score[i];
+        Arrays.sort(arr);
+        String str[] = new String[score.length];
+        for(int i=0; i<arr.length; i++) {
+            if(i==arr.length-3) 
+                str[i] = "Bronze Medal";
+            else if(i==arr.length-2) 
+                str[i] = "Silver Medal";
+            else if(i==arr.length-1) 
+                str[i] = "Gold Medal";
+            else 
+                str[i] = String.valueOf(arr.length-i);
+        }
+        String res[] = new String[score.length];
+       for(int i=0; i<score.length; i++) {
+           for(int j = 0; j < arr.length; j ++){
+               if(arr[j] == score[i]) 
+                  res[i] = str[j];
+           }
+       }
+        return res;
+    }
+}
